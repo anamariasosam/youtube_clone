72d150ba9e4c3b2663fa0657c12fdc42e4120883
Ana Maria Sosa <anamariasosam@gmail.com>
2016-12-07 17:24:23 -0500

    Add source files with some components

---
+++ src/components/app.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+const API_KEY = 'AIzaSyCYwoFZ3t5lGwGfkndc1TfAisLHLhcLlCs'
+
+export default class App extends Component {
+  render() {
+    return (
+      <h1>Hello 🍪</h1>
+    );
+  }
+}
