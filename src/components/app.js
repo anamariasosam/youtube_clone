8a776c03a3805a6583bf977b78a965a9af13b7f2
Ana Maria Sosa <anamariasosam@gmail.com>
2016-12-07 21:30:55 -0500

    Create SearchBar and VideoList components and add it to the app component

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,31 @@
 import React, { Component } from 'react';
+import YTSearch from 'youtube-api-search';
 
+import SearchBar from './SearchBar';
+import VideoList from './VideoList';
 
-const API_KEY = 'AIzaSyCYwoFZ3t5lGwGfkndc1TfAisLHLhcLlCs'
+const API_KEY = 'AIzaSyCYwoFZ3t5lGwGfkndc1TfAisLHLhcLlCs';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: [],
+    };
+
+    YTSearch({ key: API_KEY, term: 'surfboards' }, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
   render() {
     return (
-      <h1>Hello 🍪</h1>
+      <div>
+        <h1>Hello 🍪</h1>
+        <SearchBar />
+        <VideoList videos= { this.state.videos }/>
+      </div>
     );
   }
 }
